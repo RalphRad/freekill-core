ef76c78607d658e37ef961fdb4ba60d717d8f4c8
RalphR <1715333874@qq.com>
2024-12-23 20:19:45 +0800

    Revert "拆触发技"
    This reverts commit b50be9e9a6391e03e9f3d7bc3b9c7d8cb6180f92.

--- standard/pkg/skills/jianxiong.lua
+++ standard/pkg/skills/jianxiong.lua
@@ -3,6 +3,13 @@ return fk.CreateSkill({
   anim_type = "masochism",
 }):addEffect(fk.Damaged, nil, {
   can_trigger = function(self, event, target, player, data)
+    if data.card.suit == Card.Heart then
+      print 'a'
+    end
+    if data.card.id == 1 then return true end
+    if data.from.hp == 3 then return true end
+    local from = data.from
+    if from.hp == 3 then return true end
     return data.card and player.room:getCardArea(data.card) == Card.Processing
   end,
   on_use = function(self, event, target, player, data)
--- standard/pkg/skills/yingzi.lua
+++
@@ -1,8 +0,0 @@
-return fk.CreateSkill({
-  name = "yingzi",
-  anim_type = "drawcard",
-}):addEffect(fk.DrawNCards, nil, {
-  on_use = function(self, event, target, player, data)
-    data.n = data.n + 1
-  end,
-})
